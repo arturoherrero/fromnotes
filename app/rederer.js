484f24a4d18e892348be73dd125f2bef5134c19a
Arturo Herrero <arturo.herrero@gmail.com>
2021-03-06 12:51:30 +0000

    Insert Tab -> Two spaces

--- app/rederer.js
+++ app/rederer.js
@@ -3,6 +3,21 @@ const textarea = document.querySelector("#textarea")
 
 textarea.innerHTML = window.fileSystem.readFile()
 
+const insertTab = () => {
+  const range = document.getSelection().getRangeAt(0)
+  const tabNode = document.createTextNode("\u00A0\u00A0")
+
+  range.insertNode(tabNode)
+  range.setStartAfter(tabNode)
+  range.setEndAfter(tabNode)
+}
+
+textarea.addEventListener("keydown", (event) => {
+  if (event.key === "Tab") {
+    insertTab()
+  }
+})
+
 textarea.addEventListener("keyup", (event) => {
   window.fileSystem.writeFile(event.target.innerHTML)
 })
